b7f8b767315c0fde6ef8b3530164b1fa49a90329
jiayu128 <jiayu_overmars@126.com>
2021-05-25 14:01:10 +0800

    4

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x9910193a4447392a6334da160a85966ccfbcbe89";
+export declare const INIT_CODE_HASH = "ee4d2ed5837ac0e0d51484af482d089047e57bb4c7b6d533486c42058115a6b6";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
